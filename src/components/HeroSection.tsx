952a9259eb50df6837912b3a6b738edde3890ca5
Nadir AMMISAID <155676747+nadir-ammisaid@users.noreply.github.com>
2025-08-21 20:47:45 +0200

    perf(website): enhance mobile perf 96

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -21,12 +21,12 @@ export default function HeroSection() {
           fill
           priority
           fetchPriority="high"
-          sizes="(max-width: 430px) 100vw,
-                (max-width: 1024px) 100vw,
+          sizes="(max-width: 430px) 430px,
+                (max-width: 1024px) 1024px,
                 1200px"
-          quality={50}
+          quality={40}
           placeholder="blur"
-          blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nMScgaGVpZ2h0PScxJyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnLz4="
+          blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIwMCIgaGVpZ2h0PSI2MDAiIHZpZXdCb3g9IjAgMCAxMjAwIDYwMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjEyMDAiIGhlaWdodD0iNjAwIiBmaWxsPSIjMUYyOTM3Ii8+CjxyZWN0IHg9IjEwMCIgeT0iMTAwIiB3aWR0aD0iMTAwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiM2MzY2RjEiIG9wYWNpdHk9IjAuMyIvPgo8L3N2Zz4K"
         />
       </div>
 
